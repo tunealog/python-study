cdc4d81ec123d19e9a37d0fa29f3db4d9cb1289d
tunealog <tunealog@gmail.com>
2020-06-20 19:13:02 +0900

    pyex004_Types-Strings

---
+++ ex/ex004.py
@@ -0,0 +1,37 @@
+# Basic of Python
+# Project 1hour
+
+# Title : Types-Strings
+# Date : 2020-06-20
+# Creater : tunealog
+
+# Strings
+x = "Hello"
+y = "Bye"
+z = """
+Hello
+I'm tunealog
+Nice to meet you
+Bye
+"""
+
+# Hello Python
+print("Hello"+"Python")
+
+# Hello Bye
+print(x+y)
+
+# Hello ~ Bye
+print(z)
+
+# Before Casting
+# print("Age : " + 10)
+# TypeError: cannot concatenate 'str' and 'int' objects
+
+# After Casting
+print("Age : " + str(10))
+
+# Int Type Casting
+a = 10
+b = "10"
+print(a + int(b))
