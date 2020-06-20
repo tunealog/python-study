ed509d539d3928bba284d335632a0723c04a7053
tunealog <tunealog@gmail.com>
2020-06-20 18:27:24 +0900

    pyex003_Types-Numbers

---
+++ ex/ex003.py
@@ -0,0 +1,47 @@
+# Basic of Python
+# Project 1hour
+
+# Title : Types-Numbers
+# Date : 2020-06-20
+#Creater : tunealog
+
+# Number
+x = 10
+y = 1.5
+z = -1
+
+# Addition
+print("Addition")
+print(x + y)
+print(y + z)
+print(x + z)
+
+# Subtraction
+print("Subtraction")
+print(x - y)
+print(y - z)
+print(x - z)
+
+# Multiplication
+print("Multiplication")
+print(x * y)
+print(y * z)
+print(x * z)
+
+# Devision
+print("Devision")
+print(x / y)
+print(y / z)
+print(x / z)
+
+# Square
+print("Square")
+print(x ** 2)
+print(y ** 2)
+print(x ** 2)
+
+# Modular
+print("Modular")
+print(x % 2)
+print(y % 2)
+print(x % 2)
