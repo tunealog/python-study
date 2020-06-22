0be683a18d82f71ab3e4faaa67b9c8d56f16d506
tunealog <tunealog@gmail.com>
2020-06-22 20:04:57 +0900

    pyex012_Dictionary

---
+++ ex/ex012.py
@@ -0,0 +1,60 @@
+# Basic of Python
+# Project 1hour
+
+# Title : Dictionary
+# Date : 2020-06-22
+# Creater : tunealog
+
+x = {
+    0: "Hello",
+    1: "Hi",
+    "name": "tunealog",
+    "age": 20,
+}
+
+#{0: 'Hello', 1: 'Hi', 'name': 'tunealog', 'age': 20}
+print(x)
+
+# tunealog
+print(x["name"])
+
+# 20
+print(x["age"])
+
+# Hello
+print(x[0])
+
+# Hi
+print(x[1])
+
+# True
+print("age" in x)
+
+# False
+print("sex" in x)
+
+#dict_keys([0, 1, 'name', 'age'])
+print(x.keys())
+
+#dict_values(['Hello', 'Hi', 'tunealog', 20])
+print(x.values())
+
+#key : 0
+#value : Hello
+#key : 1
+#value : Hi
+#key : name
+#value : tunealog
+#key : age
+#value : 20
+for key in x:
+    print("key : " + str(key))
+    print("value : " + str(x[key]))
+
+#{0: 'bye', 1: 'Hi', 'name': 'tunealog', 'age': 20}
+x[0] = "bye"
+print(x)
+
+#{0: 'bye', 1: 'Hi', 'name': 'tunealog', 'age': 20, 'city': 'Newyork'}
+x["city"] = "Newyork"
+print(x)
