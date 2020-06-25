c48d46e367653b970a11603856417e6ac9ce0748
tunealog <tunealog@gmail.com>
2020-06-25 20:28:46 +0900

    pyex018_Char Escape

---
+++ ex/ex018.py
@@ -0,0 +1,27 @@
+# Basic of Python
+# Project 1hour
+
+# Title : Char Escape
+# Date : 2020-06-25
+# Creater : tunealog
+
+# In 1865, Nietzsche thoroughly studied
+# the works of Arthur Schopenhauer
+print("In 1865, Nietzsche thoroughly studied\nthe works of Arthur Schopenhauer")
+
+# \" or \' : " or '
+# I'm "tunealog"
+print('Im "tunalog"')
+print("Im \"tunalog\"")
+
+# \\ : \
+print("C:\\Users\\tunealog\\Desktop")
+
+# \r : Move Cursor
+print("Red Apple\rPine")
+
+# \b : Back Space(1char delete)
+print("Redd\bApple")
+
+# \t : Tab
+print("Red\tApple")
