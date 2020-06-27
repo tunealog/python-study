d701757c71774f30e101a2afc884687c5a0992ca
tunealog <tunealog@gmail.com>
2020-06-28 00:01:35 +0900

    pyex003_Types Numbers

--- ex/ex003.py
+++ ex/ex003.py
@@ -1,9 +1,12 @@
 # Basic of Python
 # Project 1hour
 
-# Title : Types-Numbers
+# Title : Types Numbers
 # Date : 2020-06-20
-#Creater : tunealog
+# Creator : tunealog
+
+from math import *
+from random import *
 
 # Number
 x = 10
@@ -45,3 +48,33 @@ print("Modular")
 print(x % 2)
 print(y % 2)
 print(x % 2)
+
+# Advenced Types
+# Added Date : 2020-06-27
+
+# 1
+print(5//3)
+
+# True
+print(10 >= 3)
+
+# 5
+print(abs(-5))
+
+# 16
+print(pow(4, 2))
+
+# 3
+print(round(3.14))
+
+# 4
+print(floor(4.99))
+print(ceil(3.14))
+print(sqrt(16))
+
+# Random
+print(random())
+print(int(random() * 10)+1)
+
+print(randrange(1, 10))
+print(randint(1, 10))
