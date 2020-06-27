b511c4059da30d70dae3fbc93fd9a1233de76297
tunealog <tunealog@gmail.com>
2020-06-27 09:33:55 +0900

    pyex001_Hello Python

--- ex/ex001.py
+++ ex/ex001.py
@@ -1,8 +1,8 @@
 # Basic of Python
 # Project 1hour
 
-# Title : Construct Develop Environment
+# Title : Hello Python
 # Date : 2020-06-20
-#Creater : tunealog
+#Creator : tunealog
 
 print("Hello Python")
