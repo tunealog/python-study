a75d8b75daf8c12b65713b3e9a8960db06bc71fd
tunealog <tunealog@gmail.com>
2020-06-29 19:56:06 +0900

    pyex026_ A variety of Output Formats

---
+++ ex/ex026.py
@@ -0,0 +1,30 @@
+# Basic of Python
+
+# Title : A variety of Output Formats
+# Date : 2020-06-29
+# Creator : tunealog
+
+#        500
+print("{0: >10}".format(500))
+#       +500
+print("{0: >+10}".format(500))
+#       -500
+print("{0: >+10}".format(-500))
+# +500______
+print("{0:_<+10}".format(500))
+# 500_______
+print("{0:_<10}".format(500))
+
+# 500,000,000,000
+print("{0:,}".format(500000000000))
+# +500,000,000,000
+print("{0:+,}".format(500000000000))
+# -500,000,000,000
+print("{0:+,}".format(-500000000000))
+# +500,000,000,000^^^^^^^^^^^^^^
+print("{0:^<+30,}".format(500000000000))
+
+# 1.666667
+print("{0:f}".format(5/3))
+# 1.67
+print("{0:.2f}".format(5/3))
