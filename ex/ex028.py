aaf57552714bb76ae0b10200a7c9ef02714f93ad
tunealog <tunealog@gmail.com>
2020-06-30 20:06:55 +0900

    pyex028_Pickle

---
+++ ex/ex028.py
@@ -0,0 +1,20 @@
+# Basic of Python
+
+# Title : Pickle
+# Date : 2020-06-30
+# Creator : tunealog
+
+import pickle
+
+# Pickle Write
+# profile_file = open("profile.pickle", "wb")
+# profile = {"name": "James", "age": 30, "hobby": ["soccer", "golf", "coding"]}
+# print(profile)
+# pickle.dump(profile, profile_file)
+# profile_file.close()
+
+# Pickle Load
+profile_file = open("profile.pickle", "rb")
+profile = pickle.load(profile_file)
+print(profile)
+profile_file.close()
