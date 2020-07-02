e6142e18a92e441ac6d6f688e60b447374081b96
tunealog <tunealog@gmail.com>
2020-07-02 19:47:26 +0900

    pyex014_Class Method Inheritance

--- ex/ex014.py
+++ ex/ex014.py
@@ -31,7 +31,6 @@ jenny.say_hello("tunealog")
 
 # Added Date : 2020-07-01
 
-
 class Unit:
     def __init__(self, name, hp, damage):
         self.name = name
@@ -52,3 +51,59 @@ wraith2 = Unit("Clocking Wraith", 80, 5)
 wraith2.clocking = True
 if wraith2.clocking == True:
     print("{0} is Clocked".format(wraith2.name))
+
+# Class
+# Method
+# Inheritance
+# Multiple Inheritance
+# Added Date : 2020-07-02
+
+
+class NormalUnit:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+
+
+class AttackUnit(NormalUnit):
+    def __init__(self, name, hp, damage):
+        NormalUnit.__init__(self, name, hp)
+        self.damage = damage
+
+    def attack(self, location):
+        print("{0} : Attack enemy on the {1} [Damage {2}]"
+              .format(self.name, location, self.damage))
+
+    def damaged(self, damage):
+        print("{0} : {1} Damaged from enemy"
+              .format(self.name, damage))
+        self.hp -= damage
+        print("{0} : Current HP is {1}"
+              .format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{0} : Broken".format(self.name))
+
+
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
+
+    def fly(self, name, location):
+        print("{0} : Flying to the {1} [Speed {2}]"
+              .format(name, location, self.flying_speed))
+
+
+class FlyableAttackUnit(AttackUnit, Flyable):
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, damage)
+        Flyable.__init__(self, flying_speed)
+
+
+firebat1 = AttackUnit("FireBat", 50, 16)
+firebat1.attack("West")
+
+firebat1.damaged(25)
+firebat1.damaged(25)
+
+valkyrie = FlyableAttackUnit("Valkyrie", 200, 6, 5)
+valkyrie.fly(valkyrie.name, "East")
