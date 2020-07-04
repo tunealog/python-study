d7953b12924cca0a4cf66377d6890bb9260d4496
tunealog <tunealog@gmail.com>
2020-07-04 22:54:32 +0900

    pyex036_Module(2nd)

---
+++ ex/ex036.py/Module/theater_module.py
@@ -0,0 +1,16 @@
+# Basic of Python
+
+# Title : Module(2nd)
+# Date : 2020-07-04
+# Creator : tunealog
+
+def price(people):
+    print("Price is {0} for {1} persons".format(people, people * 100))
+
+
+def price_morning(people):
+    print("Price is {0} for {1} persons".format(people, people * 60))
+
+
+def price_soldier(people):
+    print("Price is {0} for {1} persons".format(people, people * 40))
