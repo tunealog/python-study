89121dea81737b300e0b66e436c5837d68472921
tunealog <tunealog@gmail.com>
2020-07-05 20:37:47 +0900

    pyex036_Package(2nd)

--- ex/ex036.py/Package/main.py
+++ ex/ex036.py/Package/main.py
@@ -4,30 +4,31 @@
 # Date : 2020-07-05
 # Creator : tunealog
 
-
 import random
 import inspect
-# from travel import *
-# from travel import vietnam
-# import travel.thailand
-# from travel.thailand import ThailandPackage
+import travel.thailand
+from travel import *
+from travel import vietnam
+from travel.thailand import ThailandPackage
 
 # import travel.thailand
-# trip_to = travel.thailand.ThailandPackage()
-# trip_to.detail()
-
+trip_to = travel.thailand.ThailandPackage()
+trip_to.detail()
 
 # from travel.thailand import ThailandPackage
-# trip_to = ThailandPackage()
-# trip_to.detail()
+trip_to = ThailandPackage()
+trip_to.detail()
 
 # from travel import vietnam
-# trip_to = vietnam.VietnamPackage()
-# trip_to.detail()
+trip_to = vietnam.VietnamPackage()
+trip_to.detail()
 
 # from travel import *
-# trip_to = thailand.ThailandPackage()
-# trip_to.detail()
+trip_to = thailand.ThailandPackage()
+trip_to.detail()
 
+# inspect
+# import random
+# import inspect
 print(inspect.getfile(random))
-# print(inspect.getfile(thailand))
+print(inspect.getfile(thailand))
--- ex/ex036.py/Package/travel/vietnam.py
+++ ex/ex036.py/Package/travel/vietnam.py
@@ -1,3 +1,9 @@
+# Basic of Python
+
+# Title : Package(2nd)
+# Date : 2020-07-05
+# Creator : tunealog
+
 class VietnamPackage:
     def detail(self):
         print("[Vietnam Package] Danang Travel $600")
