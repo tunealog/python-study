f9dd606bde8000ffbea7efa64e7d1f43a8764888
tunealog <tunealog@gmail.com>
2020-07-05 16:31:08 +0900

    pyex037_Built-in Function

---
+++ ex/ex037.py
@@ -0,0 +1,19 @@
+# Basic of Python
+
+# Title : Built-in Function
+# Date : 2020-07-05
+# Creator : tunealog
+
+# input
+# age = input("How old are you?")
+# print("I'm {0} years old".format(age))
+
+# dir
+import random
+print(dir(random))
+
+lst = [1, 2, 3]
+print(dir(lst))
+
+name = "Jim"
+print(dir(name))
