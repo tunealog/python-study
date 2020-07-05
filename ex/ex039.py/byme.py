ae067d07d7b0f9758158bde8479847e22bed5e98
tunealog <tunealog@gmail.com>
2020-07-05 17:09:02 +0900

    pyex039_Quiz(Module)

---
+++ ex/ex039.py/byme.py
@@ -0,0 +1,21 @@
+# Basic of Python
+
+# Title : Quiz(Module)
+# Date : 2020-07-05
+# Creator : tunealog
+
+# Quiz) Create the Module which create the signature
+#
+# Rule1 : The module name is byme.py
+#
+# (Using Example)
+# import byme
+# byme.sign()
+#
+# (Output Example)
+# This program is made by tunealog
+# github : https://github.com/tunealog
+
+def sign():
+    print("This program is made by tunealog")
+    print("github : https://github.com/tunealog")
