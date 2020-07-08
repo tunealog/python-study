5f135d14000f1610e1a6350a2147026c0f21807d
tunealog <tunealog@gmail.com>
2020-07-08 21:41:09 +0900

    Python Study

--- ex/ex001.py
+++
@@ -1,8 +0,0 @@
-# Basic of Python
-# Project 1hour
-
-# Title : Hello Python
-# Date : 2020-06-20
-#Creator : tunealog
-
-print("Hello Python")
--- ex/ex_001_Hello_Python.py
+++
@@ -1,7 +0,0 @@
-# Basic of Python
-
-# Title : Hello Python
-# Date : 2020-06-20
-# Creator : tunealog
-
-print("Hello Python")
