0ccb16b1e2772bf6fc54986295f750846705774b
tunealog <tunealog@gmail.com>
2020-07-23 14:29:12 +0900

    Python Study

--- ex/ex_013_Class_Inheritance.py
+++ ex/ex_013_Class_Inheritance.py
@@ -6,12 +6,10 @@
 
 class person:
 
-    # Function : __init__
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
-    # Function : say_hello
     def say_hello(self, to_name):
         print("Hello " + to_name + " I'm " +
               self.name + " " + self.age + " years old")
@@ -30,8 +28,9 @@ jenny.say_hello("tunealog")
 
 
 # Added Date : 2020-07-01
+#########################################################################
 
-class Unit:
+class Unit_1:
     def __init__(self, name, hp, damage):
         self.name = name
         self.hp = hp
@@ -40,26 +39,34 @@ class Unit:
         print(f"HP {self.hp}, Damage {self.damage}")
 
 
-marine1 = Unit("Marine", 40, 5)
-marine2 = Unit("Marine", 40, 5)
-tank = Unit("Tank", 150, 35)
+marine1 = Unit_1("Marine", 40, 5)
+marine2 = Unit_1("Marine", 40, 5)
+tank = Unit_1("Tank", 150, 35)
 
-wraith1 = Unit("Wraith", 80, 5)
-print("Unit Name : {0}, Damage : {1}".format(wraith1.name, wraith1.damage))
+wraith1 = Unit_1("Wraith", 80, 5)
+print("Unit_1 Name : {0}, Damage : {1}".format(wraith1.name, wraith1.damage))
 
-wraith2 = Unit("Clocking Wraith", 80, 5)
+
+#########################################################################
+
+# Add Parameter
+wraith2 = Unit_1("Clocking Wraith", 80, 5)
 wraith2.clocking = True
+
 if wraith2.clocking == True:
+    # error
+    # if wraith1.clocking == True:
     print("{0} is Clocked".format(wraith2.name))
 
-# Class
-# Method
-# Inheritance
-# Multiple Inheritance
+#########################################################################
+
+# Classes
+# Inheritances
+# Multiple Inheritances
 # Added Date : 2020-07-02
 
 
-class NormalUnit:
+class Unit:
     def __init__(self, name, hp, speed):
         self.name = name
         self.hp = hp
@@ -71,9 +78,9 @@ class NormalUnit:
               .format(self.name, location, self.speed))
 
 
-class AttackUnit(NormalUnit):
+class AttackUnit(Unit):
     def __init__(self, name, hp, speed, damage):
-        NormalUnit.__init__(self, name, hp, speed)
+        Unit.__init__(self, name, hp, speed)
         self.damage = damage
 
     def attack(self, location):
@@ -104,9 +111,9 @@ class FlyableAttackUnit(AttackUnit, Flyable):
         AttackUnit.__init__(self, name, hp, 0, damage)
         Flyable.__init__(self, flying_speed)
 
-    # def move(self, location):
-    #     print("Unit moves")
-    #     self.fly(self.name, location)
+    def move(self, location):
+        print("Unit moves")
+        self.fly(self.name, location)
 
 # firebat1 = AttackUnit("FireBat", 50, 16)
 # firebat1.attack("West")
@@ -114,40 +121,44 @@ class FlyableAttackUnit(AttackUnit, Flyable):
 # firebat1.damaged(25)
 # firebat1.damaged(25)
 
-# valkyrie = FlyableAttackUnit("Valkyrie", 200, 6, 5)
-# valkyrie.fly(valkyrie.name, "East")
+
+valkyrie = FlyableAttackUnit("Valkyrie", 200, 6, 5)
+valkyrie.fly(valkyrie.name, "East")
 
 # Overloading
+# Move Command : Vulture = move / Battlecruiser = fly
+# Create def move in def fly
 
 
-# vulture = AttackUnit("Vulture", 80, 10, 20)
-# battlecruiser = FlyableAttackUnit("BattleCruiser", 500, 25, 3)
+vulture = AttackUnit("Vulture", 80, 10, 20)
+battlecruiser = FlyableAttackUnit("BattleCruiser", 500, 25, 3)
 
-# vulture.move("West")
-# # battlecruiser.fly(battlecruiser.name, "East")
-# battlecruiser.move("East")
+vulture.move("West")  # move
+# battlecruiser.fly(battlecruiser.name, "East") # fly
+battlecruiser.move("East")  # move in fly
 
 # Pass
+# Super
 # Added Date : 2020-07-03
 
-# class BuildingUnit(NormalUnit):
-#     def __init__(self, name, hp, location):
-#         pass
 
-# supply_depot = BuildingUnit("Supply Depot", 500, "West")
-
-# def game_start():
-#     print("[Alert] The new games beginning")
-
-# def game_over():
-#     pass
-
-# game_start()
-# game_over
-
-# Super
-class BuildingUnit(NormalUnit):
+class BuildingUnit(Unit):
     def __init__(self, name, hp, location):
-        # Unit.__init__(self, name, hp, 0)
+        # Unit.__init__(self, name, hp, 0) # Possible
         super().__init__(name, hp, 0)  # Can't Using for Multiple Inheritance
         self.location = location
+
+
+supply_depot = BuildingUnit("Supply Depot", 500, "West")
+
+
+def game_start():
+    print("[Alert] The new games beginning")
+
+
+def game_over():
+    pass
+
+
+game_start()
+game_over()  # pass
