6e242664571fb12ce9776c2dac7e347530eb28fc
tunealog <tunealog@gmail.com>
2020-08-03 21:50:54 +0900

    Python Study

--- ex/ex_023_Standard_Input_Output.py
+++ ex/ex_023_Standard_Input_Output.py
@@ -16,18 +16,19 @@ print("python", "Java", file=sys.stdout)
 # Standard error
 print("python", "Java", file=sys.stderr)
 
+# ljust, rjust
 scores = {"math": 0, "english": 50, "computer": 100}
 for subject, score in scores.items():
-    #    print(subject, score)
     # math    :   0
     # english :  50
     # computer: 100
     print(subject.ljust(8), str(score).rjust(4), sep=":")
 
+# zfill
 for num in range(1, 11):
     print("Waiting Number : " + str(num).zfill(3))
 
+# <class 'type'>
 answer = input("Input anything: ")
-# Input type is string
 print(type(answer))
 print("Result is " + answer + "!")
